b3b495104d48856a0fd39212b874bc00a07bc217
bearsharks <bearsharks02@gmail.com>
2022-12-31 16:46:00 +0900

    청구내역 출력함수 분리

--- statement.js
+++ statement.js
@@ -21,15 +21,20 @@ function calcCredit(numOfAudience, type) {
   return volumeCredits;
 }
 
+const format = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+}).format;
+
+function printDetails(playName, price, numOfAudience) {
+  return `  ${playName}: ${format(price / 100)} (${numOfAudience}석)\n`
+}
+
 export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
@@ -52,9 +57,7 @@ export function statement(invoice, plays) {
     volumeCredits += calcCredit(perf.audience, play.type);
 
     // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
-    }석)\n`;
+    result += printDetails(play.name, thisAmount, perf.audience);
   }
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
