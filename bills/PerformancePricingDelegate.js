242817bfb987917f4975f31287b85f12ce436176
bearsharks <bearsharks02@gmail.com>
2023-01-01 11:54:58 +0900

    공연가격계산대리자 슈퍼클래스 생성

--- bills/PerformancePricingDelegate.js
+++ bills/PerformancePricingDelegate.js
@@ -1,12 +1,20 @@
 import {band} from "../util.js";
 
-export class TragedyPerformancePricingDelegate {
+class PerformancePricingDelegate {
     #cost;
     #credits;
-
     constructor(numOfAudience) {
-        this.#cost = 40000 + band(numOfAudience, 30, Infinity) * 1000;
-        this.#credits = Math.max(numOfAudience - 30, 0);
+        this.#cost = this._calcCost(numOfAudience);
+        this.#credits = this._calcCredits(numOfAudience);
+    }
+
+    _calcCost(numOfAudience) {
+        throw new Error(`추상메소드 calcCost() 를 재정의 하지 않고 사용함`);
+        return numOfAudience;
+    }
+
+    _calcCredits(numOfAudience) {
+        return Math.max(numOfAudience - 30, 0);
     }
 
     get cost() {
@@ -18,15 +26,23 @@ export class TragedyPerformancePricingDelegate {
     }
 }
 
-
-export class ComedyPerformancePricingDelegate {
-    #cost;
-    #credits;
+export class TragedyPerformancePricingDelegate extends PerformancePricingDelegate{
     constructor(numOfAudience) {
-        this.#cost = this.calcCost(numOfAudience);
-        this.#credits = Math.max(numOfAudience - 30, 0) + Math.floor(numOfAudience / 5);
+        super(numOfAudience);
     }
-    calcCost(numOfAudience) {
+
+    _calcCost(numOfAudience) {
+        return 40000 + band(numOfAudience, 30, Infinity) * 1000;
+    }
+}
+
+
+export class ComedyPerformancePricingDelegate extends PerformancePricingDelegate{
+    constructor(numOfAudience) {
+        super(numOfAudience);
+    }
+
+    _calcCost(numOfAudience) {
         const basePrice = 30000 + (300 * numOfAudience);
         if (numOfAudience > 20) {
             const additionalCost = 10000 + 500 * band(numOfAudience, 20, Infinity);
@@ -35,12 +51,8 @@ export class ComedyPerformancePricingDelegate {
         return basePrice;
     }
 
-    get cost() {
-        return this.#cost;
-    }
-
-    get credits() {
-        return this.#credits;
+    _calcCredits(numOfAudience) {
+        return super._calcCredits(numOfAudience) + Math.floor(numOfAudience / 5);
     }
 }
 
